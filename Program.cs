849a7000cc7fe8b7eaf6ee54ae45e887393da4cc
janairorobert7 <pasionrobert7@gmail.com>
2024-04-18 10:06:44 -0700

    First Commit

--- Program.cs
+++ Program.cs
@@ -3,4 +3,31 @@
 
 using System;
 
-Console.WriteLine("Hello World");
+class ConnectFour
+{
+    private const int Rows = 6;
+    private const int Columns = 7;
+    private char[,] board = new char[Rows, Columns];
+    private char currentPlayer = 'X';
+
+    public ConnectFour()
+    {
+        InitializeBoard();
+    }
+
+    private void InitializeBoard()
+    {
+        for (int row = 0; row < Rows; i++)
+        {
+            for(int col= 0; col < Columns; j++)
+            {
+                board[row, col] = ' ';
+            }
+        }
+    }
+
+    private void PrintBoard(char[] board)
+    {
+
+    }
+}
